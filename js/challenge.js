f5ba75e0894770683018f1aa1fdc49dfd37d391c
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-20 13:10:23 -0400

    Create a basic counter and limit to 20 counts for testing

---
+++ js/challenge.js
@@ -0,0 +1,19 @@
+// every second, I need the counter to go up by 1
+// I need to start at 0, then add 1 and continue infinitely
+
+// I can add or subtract from the counter using the + or - button
+
+// I can pause the counter which will disable all buttons except the pause button
+// the pause button should switch to a label that says resume and then resume 
+
+const counterID = document.getElementById('counter')
+let storeCount = 0
+counterID.textContent = storeCount
+
+const letsCount = setInterval(function() {
+  storeCount++
+  counter.textContent = storeCount
+  if (storeCount === 20) {
+    clearInterval(letsCount)
+  }
+}, 1000)
