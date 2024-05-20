f1b30caf8aa6c8ca84f21623da86b5ab8c4a3a67
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-20 11:52:18 -0400

    Add comment section

--- js/challenge.js
+++ js/challenge.js
@@ -1,26 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
-
 let counter = document.getElementById('counter')
-
-//Start timer count
-// const startTimer = setInterval(() => {
-//   counter.innerText++
-// }, '1000')
-
-
-// function startTimer() {
-//   setInterval(() => {
-//     counter.innerText++
-//   }, '1000')
-// }
-
-
-//Manually increment timer
-//Grab timer button
-//Add on click
-//On click manually increase timer
 let plus = document.getElementById('plus')
 let minus = document.getElementById('minus')
+let timer
+let form = document.getElementById('comment-form')
+
+
+function startTimer() {
+  timer = setInterval(() => {
+    counter.innerText++;
+  }, 1000)
+}
+  startTimer()
 
 plus.addEventListener('click', (e) => {
   counter.innerText++
@@ -36,7 +27,6 @@ let likeCounter = null
 let currentLikes = 0
 
 heart.addEventListener('click', (e) => {
-  // console.log(counter.innerText)
   let li = document.createElement('li')
   let likesSection = document.querySelector('.likes')
   let counterValue = counter.innerText;
@@ -52,49 +42,36 @@ heart.addEventListener('click', (e) => {
   likesSection.appendChild(li)
   })
 
-  //Pause Counter
-  //Disable all buttons except pause
-  //Switch label from pause to resume 
-
-  //Grab Pause Button
-  //On Click Event Listener 
-  //set clearInterval
-  //Change innerText to resume
-  //Add Disable property to buttons
-  //Create conditional
-  //if innerText = pause
-  //else if innerText = Resume
-  //Remove disable
-  //Restart timer
-
-  let timer
-
-  function startTimer() {
-    timer = setInterval(() => {
-      counter.innerText++;
-    }, 1000)
-  }
-    startTimer()
   
   let pause = document.getElementById('pause')
+  let submit = document.getElementById('submit')
 
   pause.addEventListener('click', (e) => {
     if (pause.innerText === 'pause') {
       clearInterval(timer)
       pause.innerText = 'resume'
       heart.disabled = true
+      minus.disabled = true
+      plus.disabled = true
+      submit.disabled = true
     } else if (pause.innerText === 'resume') {
       startTimer()
       pause.innerText = 'pause'
       heart.disabled = false
-
+      minus.disabled = false
+      plus.disabled = false
+      submit.disabled = true
     }
-  });
+})
 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let comment = e.target.comment.value
+    let list = document.getElementById('list')
+    let p = document.createElement('p')
 
-
-
-
-
-
+    p.innerText = comment
+    list.appendChild(p)
+    form.reset()
+  })
 });
