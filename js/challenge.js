69cd552bdada223ad8c6e297827efa9696ef0e97
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-20 16:54:29 -0400

    Fix placement of liked items, clean up consts

--- js/challenge.js
+++ js/challenge.js
@@ -10,44 +10,45 @@
 
 document.addEventListener("DOMContentLoaded", (e) => {
   const counterID = document.getElementById('counter')
+  const pauseButton = document.getElementById('pause')
+  const minusButton = document.getElementById('minus')
+  const plusButton = document.getElementById('plus')
+  const heartButton = document.getElementById('heart')
+  const likeSection = document.querySelector('.likes')
+
   let storeCount = 0
-  
+
   function startCount(interval) {
     return setInterval(function() {
       storeCount++
       counterID.textContent = storeCount
     }, interval)
+
   }
   letsCount = startCount(1000);
   
-  const pauseButton = document.getElementById('pause')
-
   pauseButton.addEventListener('click', (e) => {
     if (pauseButton.innerText === 'pause') {
       pauseButton.innerText = 'resume'
-      clearInterval(letsCount)  
+      clearInterval(letsCount)
     } else {
       startCount(1000)
       pauseButton.innerText = 'pause'
     }
   })
 
-  const minusButton = document.getElementById('minus')
   minusButton.addEventListener('click', (e) => {
     storeCount--
   })
 
-  const plusButton = document.getElementById('plus')
   plusButton.addEventListener('click', (e) => {
     storeCount++
   })
 
-  const heartButton = document.getElementById('heart')
   heartButton.addEventListener('click', (e) => {
-    const likeSection = document.getElementsByClassName('likes')
     const li = document.createElement('li')
     li.textContent = `${storeCount} has been liked x time(s)`
-    document.body.appendChild(li)
+    likeSection.appendChild(li)
   })
 
 })
