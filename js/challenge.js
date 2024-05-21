5d3b5e98388f7013854e0f7c93d77e0ad861250f
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-20 20:59:57 -0400

    Add ability to submit form and clean up code

--- js/challenge.js
+++ js/challenge.js
@@ -1,22 +1,15 @@
-// // every second, I need the counter to go up by 1 ✔️
-// // I need to start at 0, then add 1 and continue infinitely ✔️
-
-// // I can add or subtract from the counter using the + or - button ✔️
-// // this is done while the counter is working ✔️
-
-// // I can pause the counter ✔️
-// // When I pause, I will disable all buttons except the pause button
-// // the pause button should switch to a label that says resume and then restart to resume ✔️
-
 document.addEventListener("DOMContentLoaded", (e) => {
+  
   const counterID = document.getElementById('counter')
   const pauseButton = document.getElementById('pause')
   const minusButton = document.getElementById('minus')
   const plusButton = document.getElementById('plus')
   const heartButton = document.getElementById('heart')
   const likeSection = document.querySelector('.likes')
+  const submitButton = document.getElementById('submit')
+  const formInput = document.getElementById('comment-input')
+  const commentSection = document.getElementById('list')
   let intervalID = startCount(1000);
-
   let storeCount = 0
 
   function startCount(interval) {
@@ -27,19 +20,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
   }
   
   pauseButton.addEventListener('click', (e) => {
-  
     if (pauseButton.innerText === 'pause') {
       pauseButton.innerText = 'resume'
       clearInterval(intervalID)
       minusButton.disabled = true
       plusButton.disabled = true
       heartButton.disabled = true
+      submitButton.disabled = true
     } else {
       intervalID = startCount(1000)
       pauseButton.innerText = 'pause'
       minusButton.disabled = false
       plusButton.disabled = false
       heartButton.disabled = false
+      submitButton.disabled = false
     }
   })
 
@@ -57,5 +51,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
     likeSection.appendChild(li)
   })
 
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    const li = document.createElement('li')
+    li.textContent = formInput.value
+    commentSection.appendChild(li)
+  })
 })
-
